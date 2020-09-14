53f659e250ce921b1342c442427b1e2d0a2254a7
MaksLachugin <71094259+MaksLachugin@users.noreply.github.com>
2020-09-14 12:35:52 +0300

    test1

---
+++ src/ru/vsu/cs/kg2020/g102/lachugin_m_d/t1/DrawPanel.java
@@ -0,0 +1,14 @@
+package ru.vsu.cs.kg2020.g102.lachugin_m_d.t1;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DrawPanel extends JPanel {
+
+    @Override
+    public void paint(Graphics g) {
+        Graphics2D gr = (Graphics2D) g;
+
+    }
+}
+
