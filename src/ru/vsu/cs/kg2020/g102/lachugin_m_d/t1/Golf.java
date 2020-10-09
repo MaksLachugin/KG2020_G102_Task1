648f81dd0ff45a1f2e7557ee79cd573c9b6e56d1
MaksLachugin <71094259+MaksLachugin@users.noreply.github.com>
2020-10-10 00:15:57 +0300

    rereBody windshield1

--- src/ru/vsu/cs/kg2020/g102/lachugin_m_d/t1/Golf.java
+++ src/ru/vsu/cs/kg2020/g102/lachugin_m_d/t1/Golf.java
@@ -37,19 +37,19 @@ public class Golf implements Drawable {
     private void body() {
 
         // основные точки корпуса
-        double[] arrX = new double[]{23, 28, 33, 36, 40, 41, 42, 43, 45, 49, 52.5, 56, 59, 63, 64.5, 65.5, 66.5, 67, 67.5,68, 67.8, 67.5, 67, 66};
-        double[] arrY = new double[]{2, 2.5, 3, 3.5, 6, 7, 9.5, 12, 16.5, 25.5, 33.5, 41.5, 48, 57, 62,64.5,66.5,68, 72.5, 76, 78, 81,84, 86};
+        double[] arrX = new double[]{23, 28, 33, 36, 40, 41, 42, 43, 45, 49, 52.5, 56, 59, 63, 64.5, 65.5, 66.5, 67, 67.5, 68, 67.8, 67.5, 67, 66, 59, 57, 49, 42, 35, 28};
+        double[] arrY = new double[]{2, 2.5, 3, 3.5, 6, 7, 9.5, 12, 16.5, 25.5, 33.5, 41.5, 48, 57, 62, 64.5, 66.5, 68, 72.5, 76, 78, 81, 84, 86, 101, 103, 105, 106, 107, 107.5};
         drawMirror.setPolygon(arrX, arrY);
         drawMirror.draw(g);
-//        windshield();
+        windshield();
 //        hood();
 
 
     }
 
     private void windshield() {
-        double[] arrX = new double[]{28, 35, 37, 40, 42, 49, 50, 49};
-        double[] arrY = new double[]{6, 7, 8, 10, 14, 31, 35, 36};
+        double[] arrX = new double[]{7, 14, 21, 28, 35, 36, 37, 38, 39, 40.5, 41.5, 48,49,49.5,49,48,47,14};
+        double[] arrY = new double[]{5.5, 5.7, 5.8, 6, 6.5, 6.7, 7, 7.7, 9, 11, 14,30,32.5,34,35,36,36.5,37};
         drawMirror.setColor(Color.cyan);
         drawMirror.setPolygon(arrX, arrY);
         drawMirror.draw(g);
